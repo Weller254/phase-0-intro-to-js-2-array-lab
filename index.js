c5d840eb31df534140a9fd4987bd872ede9fc4d5
Kelvin Muturi <muturikelvin893@gmail.com>
2023-03-18 23:15:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+cats;
+
+function destructivelyAppendCat(){
+    return (cats.push("Ralph"));
+}
+function destructivelyPrependCat(){
+    return (cats.unshift("Bob"));
+}
+function destructivelyRemoveLastCat(){
+    return (cats.pop());
+}
+function destructivelyRemoveFirstCat(){
+    return (cats.shift());
+}
+function appendCat(){
+    const copyCatsLast = [...cats,"Broom"];
+    return copyCatsLast;
+}
+function prependCat(){
+    const copyCatsFirst = ["Arnold",...cats]
+    return (copyCatsFirst);
+}
+function removeLastCat(){
+    const removeLast = cats.slice(0,2)
+    return removeLast;
+}
+function removeFirstCat(){
+    const removeFirst = cats.slice(1,3);
+    return removeFirst;
+}
